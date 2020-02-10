78fae6cc5a060f131e95391ac8dac56ba4f0f5b9
Kanahiro <mediterranean1769@gmail.com>
2020-02-10 20:22:51 +0900

    first

---
+++ src/index.js
@@ -0,0 +1,34 @@
+let mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2FuYWhpcm8iLCJhIjoiY2s2NG96bzE5MHoxdzNrbW04NHB3ZHdxYyJ9.Vm6lPukISVr9c9pIUstsiQ'
+
+let map = new mapboxgl.Map({
+    'container':'mapPane',
+    'style':{
+        'version':8,
+        'sources':{
+            'gsi-bvmap':{
+                'type':'vector',
+                'tiles':[
+                    'https://cyberjapandata.gsi.go.jp/xyz/experimental_bvmap/{z}/{x}/{y}.pbf'
+                ],
+            },
+            'osm':{
+                'type':'raster',
+                'tiles':['https://tile.openstreetmap.jp/{z}/{x}/{y}.png']
+            }
+        },
+        'layers':[
+            {
+                'id':'road',
+                'type':'line',
+                'source':'gsi-bvmap',
+                'source-layer':'road',
+                'paint':{
+                    'line-color':'#000000'
+                }
+            }
+        ]
+    },
+    center: [139.767, 35.681],
+    zoom: 9
+})
